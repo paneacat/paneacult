8a8ec5f86659951fe4b9b596d202a2cb9fe16029
Patrizia <57713356+paneacat@users.noreply.github.com>
2026-04-25 13:46:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 const filtri = document.querySelectorAll('.filtro');
+const generi = document.querySelectorAll('.genere');
 const cards = document.querySelectorAll('.card');
 
+// CATEGORIE
 filtri.forEach(btn => {
   btn.addEventListener('click', () => {
-    
-    const filtro = btn.dataset.filter;
 
-    // attivo visivo
     filtri.forEach(b => b.classList.remove('attivo'));
     btn.classList.add('attivo');
+
+    const filtro = btn.dataset.filter;
 
     cards.forEach(card => {
       const categoria = card.dataset.categoria;
 
       if (filtro === "tutti" || categoria === filtro) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+
+  });
+});
+
+// GENERI
+generi.forEach(btn => {
+  btn.addEventListener('click', () => {
+
+    generi.forEach(b => b.classList.remove('attivo'));
+    btn.classList.add('attivo');
+
+    const genere = btn.dataset.genere;
+
+    cards.forEach(card => {
+      const cardGenere = card.dataset.genere;
+
+      if (genere === "tutti" || cardGenere.includes(genere)) {
         card.style.display = "block";
       } else {
         card.style.display = "none";
