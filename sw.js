50324a07564870f2bfe8ab17f986a6234659855b
Patrizia C. <57713356+paneacat@users.noreply.github.com>
2026-05-04 09:13:59 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "paneacult-v1"
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/script.js"
+      ])
+    })
+  )
+})
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request)
+    })
+  )
+})
