bccc07eb59a272878953763308d7a59c4b41205d
Patrizia C. <57713356+paneacat@users.noreply.github.com>
2026-05-09 19:44:36 +0200

    Increase splash transition duration to 2.4 seconds

--- script.js
+++ script.js
@@ -173,7 +173,7 @@ document.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => {
 
       splash.style.opacity = "0";
-      splash.style.transition = "0.4s";
+      splash.style.transition = "2.4s";
 
       setTimeout(() => {
 
