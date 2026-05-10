e8423319f59c43091a7c9f4ccae4f2adcece5d72
Patrizia C. <57713356+paneacat@users.noreply.github.com>
2026-05-10 18:15:50 +0200

    Update scriptapp.js

--- scriptapp.js
+++ scriptapp.js
@@ -2,7 +2,7 @@ const supabaseUrl =
   "https://czvtirkuyhcilmzbwysf.supabase.co";
 
 const supabaseKey =
-  "TUA_KEY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6dnRpcmt1eWhjaWxtemJ3eXNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczOTM1NjIsImV4cCI6MjA5Mjk2OTU2Mn0.v--ZBxJyMAIpb1bWbN6J3DUDi5FfcoOrhKccwRyuEvw";
 
 const supabaseClient =
   supabase.createClient(
