92fe9e55c648358b21d2ae81232a58b24dda9c1b
Patrizia C. <57713356+paneacat@users.noreply.github.com>
2026-05-16 17:36:21 +0200

    Implement service worker activate event
    Add activate event listener to handle cache deletion.

--- sw.js
+++ sw.js
@@ -3,7 +3,34 @@ const CACHE = "paneacult-v2";
 self.addEventListener(
   "install",
   e => {
+self.addEventListener(
+  "activate",
+  e => {
 
+    e.waitUntil(
+
+      caches.keys().then(keys => {
+
+        return Promise.all(
+
+          keys.map(key => {
+
+            if(key !== CACHE){
+
+              return caches.delete(key);
+
+            }
+
+          })
+
+        );
+
+      })
+
+    );
+
+  }
+);
     e.waitUntil(
 
       caches.open(CACHE).then(cache => {
