9c76b8f4494fc313a2613b7ae238bcbbc5818373
Patrizia C. <57713356+paneacat@users.noreply.github.com>
2026-05-16 15:12:36 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -10,22 +10,6 @@ const supabaseClient =
     supabaseKey
   );
 
-document.addEventListener("DOMContentLoaded", async () => {
-const {
-  data: { session }
-} = await supabaseClient.auth.getSession();
-
-if(
-  session &&
-  window.location.pathname.includes("login")
-){
-
-  window.location.href =
-    "/home.html";
-
-  return;
-}
-  
   
   const supabaseUrl = "https://czvtirkuyhcilmzbwysf.supabase.co/rest/v1/";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6dnRpcmt1eWhjaWxtemJ3eXNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczOTM1NjIsImV4cCI6MjA5Mjk2OTU2Mn0.v--ZBxJyMAIpb1bWbN6J3DUDi5FfcoOrhKccwRyuEvw";
