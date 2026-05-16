c4f38dc28a48a5f81ae303ed95a4c6f90b0d0b80
Patrizia C. <57713356+paneacat@users.noreply.github.com>
2026-05-16 17:32:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,54 @@
-const CACHE = "paneacult-v1"
+const CACHE = "paneacult-v2";
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/script.js"
-      ])
-    })
-  )
-})
+self.addEventListener(
+  "install",
+  e => {
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request)
-    })
-  )
-})
+    e.waitUntil(
+
+      caches.open(CACHE).then(cache => {
+
+        return cache.addAll([
+
+          "/",
+          "/index.html",
+
+          "/css/base.css",
+          "/css/components.css",
+          "/css/home.css",
+          "/css/mobile.css",
+
+          "/js/ui.js",
+          "/js/components.js",
+          "/js/slider.js",
+
+          "/manifest.json",
+
+          "/img/logo.webp"
+
+        ]);
+
+      })
+
+    );
+
+  }
+);
+
+
+self.addEventListener(
+  "fetch",
+  e => {
+
+    e.respondWith(
+
+      caches.match(e.request).then(res => {
+
+        return res || fetch(e.request);
+
+      })
+
+    );
+
+  }
+);
