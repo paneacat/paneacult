3848d3d2e27577a607c777654e3aabe45e0734ab
Patrizia C. <57713356+paneacat@users.noreply.github.com>
2026-05-18 15:56:32 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,19 +2,10 @@ const supabaseUrl =
   "https://czvtirkuyhcilmzbwysf.supabase.co";
 
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6dnRpcmt1eWhjaWxtemJ3eXNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczOTM1NjIsImV4cCI6MjA5Mjk2OTU2Mn0.v--ZBxJyMAIpb1bWbN6J3DUDi5FfcoOrhKccwRyuEvw";
+  "TUA_CHIAVE";
 
 const supabaseClient =
   supabase.createClient(
     supabaseUrl,
     supabaseKey
   );
-
-  
-  const supabaseUrl = "https://czvtirkuyhcilmzbwysf.supabase.co/rest/v1/";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6dnRpcmt1eWhjaWxtemJ3eXNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczOTM1NjIsImV4cCI6MjA5Mjk2OTU2Mn0.v--ZBxJyMAIpb1bWbN6J3DUDi5FfcoOrhKccwRyuEvw";
-
-const supabaseClient = supabase.createClient(
-  supabaseUrl,
-  supabaseKey
-);
