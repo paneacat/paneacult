88ae613cc600f1c4c8c5c8290118a4deee3d33d8
Patrizia C. <57713356+paneacat@users.noreply.github.com>
2026-06-23 16:00:51 +0200

    Add activate event listener to service worker

--- sw.js
+++ sw.js
@@ -2,4 +2,8 @@ self.addEventListener("install", (event) => {
   console.log("Service Worker installato");
 });
 
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker attivato");
+});
+
 self.addEventListener("fetch", (event) => {});
