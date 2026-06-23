db1b1137abd0ed89b8cf85305b36dc49a4ae7c8f
Patrizia C. <57713356+paneacat@users.noreply.github.com>
2026-06-23 15:31:06 +0200

    Create manifest.js

---
+++ js/manifest.js
@@ -0,0 +1,13 @@
+{
+  "name": "paneacult",
+  "background_color": "#fdc103",
+  "theme_color": "#fdc103",
+  "display": "standalone",
+  "icons": [
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
